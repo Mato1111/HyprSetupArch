ec641c4f23b3e083a17e3051df66f3fc29b68781
Mato1111 <ratoteamyt@gmail.com>
2023-11-19 22:22:50 -0500

    sync

--- Local Sync/sync.sh
+++ Local Sync/sync.sh
@@ -3,7 +3,9 @@
 
 rsync ~/.config/hypr/hyprland.conf /HDD1/HyprSetupArch/hypr/hyprland.conf
 rsync -r --del ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
-rsync ~/.config/WallpaperSelect/wallpaper.txt /HDD1/HyprSetupArch/WallpaperSelect/
+rsync ~/.config/WallpaperSelect/wallpaper.txt /HDD1/HyprSetupArch/WallpaperSelect/wallpaper.txt
+rsync ~/.config/qt5ct/qt5ct.conf /HDD1/HyprSetupArch/Theme/qt5ct/qt5ct.conf
+rsync ~/.config/Kvantum/kvantum.kvconfig /HDD1/HyprSetupArch/Theme/Kvantum/kvantum.kvconfig
 
 zip -FS /HDD1/HyprSetupArch/Wallpapers/wallpapers.zip /HDD1/HyprSetupArch/Wallpapers/Wallpapers/*
 
--- install.sh
+++ install.sh
@@ -5,11 +5,18 @@ yay -S wget --noconfirm
 mkdir ~/.config/hypr/
 cd ~/.config/hypr/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupArch/main/hypr/hyprland.conf
 
+
 mkdir ~/Pictures/Wallpapers/
 cd ~/Pictures/Wallpapers/ && wget https://github.com/Mato1111/HyprSetupArch/raw/main/Wallpapers/wallpapers.zip
 
 mkdir ~/.config/waypaper/
 cd ~/.config/~/.config/waypaper/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupArch/main/waypaper/config.ini
+
+mkdir ~/.config/Kvantum/
+cd ~/.config/Kvantum/ && 
+
+mkdir ~/.config/qt5ct/
+cd ~/.config/qt5ct/ && wget 
 
 yay -S hyprland-git waybar-hyprland-git dunst copyq python python-pyqt5 ffmpeg linux-wallpaperengine-git waypaper swaybg polkit-kde-agent qt5ct kvantum layan-gtk-theme-git --noconfirm
 
@@ -21,5 +28,4 @@ cd ~/.config/WallpaperSelect/ && wget
 
 cd ~/ && wget https://raw.githubusercontent.com/vinceliuice/Layan-kde/master/install.sh && bash install.sh
 gsettings set org.gnome.desktop.interface gtk-theme "Layan-Dark"
-kvantummanager
-qt5ct
+
