d9e56fcc7ecf6aa997d5c68c7ca6c0c3278f15aa
Mato1111 <ratoteamyt@gmail.com>
2023-11-20 01:34:00 -0500

    sync

--- Local Sync/sync.sh
+++ Local Sync/sync.sh
@@ -6,6 +6,8 @@ rsync -r --del ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
 rsync ~/.config/WallpaperSelect/wallpaper.txt /HDD1/HyprSetupArch/WallpaperSelect/wallpaper.txt
 rsync ~/.config/qt5ct/qt5ct.conf /HDD1/HyprSetupArch/Theme/qt5ct/qt5ct.conf
 rsync ~/.config/Kvantum/kvantum.kvconfig /HDD1/HyprSetupArch/Theme/Kvantum/kvantum.kvconfig
+rsync ~/.config/waybar/config.jsonc /HDD1/HyprSetupArch/waybar/config.jsonc
+rsync ~/.config/waybar/style.css /HDD1/HyprSetupArch/waybar/style.css
 
 zip -FS /HDD1/HyprSetupArch/Wallpapers/wallpapers.zip /HDD1/HyprSetupArch/Wallpapers/Wallpapers/*
 
