024942070d41f6fc5483082678960e773e03207b
Mato1111 <ratoteamyt@gmail.com>
2023-11-19 20:00:11 -0500

    sync

--- Local Sync/sync.sh
+++ Local Sync/sync.sh
@@ -4,4 +4,4 @@
 rsync ~/.config/hypr/hyprland.conf /HDD1/HyprSetupArch/hypr/hyprland.conf
 rsync ~/.config/waypaper/config.ini /HDD1/HyprSetupArch/waypaper/config.ini
 rsync -r --del ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
-cd /HDD1/HyprSetupArch/ && git push
+cd /HDD1/HyprSetupArch/ && git commit -m "sync" -a && git push
