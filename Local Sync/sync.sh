677b4d0fecb448ac5ce301d7195c156c18de525b
Mato1111 <ratoteamyt@gmail.com>
2023-11-20 01:46:34 -0500

    sync

--- Local Sync/sync.sh
+++ Local Sync/sync.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+# Configs
 rsync ~/.config/hypr/hyprland.conf /HDD1/HyprSetupArch/hypr/hyprland.conf
-rsync -r --del ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
 rsync ~/.config/WallpaperSelect/wallpaper.txt /HDD1/HyprSetupArch/WallpaperSelect/wallpaper.txt
 rsync ~/.config/qt5ct/qt5ct.conf /HDD1/HyprSetupArch/Theme/qt5ct/qt5ct.conf
 rsync ~/.config/Kvantum/kvantum.kvconfig /HDD1/HyprSetupArch/Theme/Kvantum/kvantum.kvconfig
 rsync ~/.config/waybar/config.jsonc /HDD1/HyprSetupArch/waybar/config.jsonc
 rsync ~/.config/waybar/style.css /HDD1/HyprSetupArch/waybar/style.css
 
-zip -FS /HDD1/HyprSetupArch/Wallpapers/wallpapers.zip /HDD1/HyprSetupArch/Wallpapers/Wallpapers/*
+# Wallpapers
+rsync -r --del ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
+zip -FS /HDD1/HyprSetupArch/Wallpapers/wallpapers978543.zip /HDD1/HyprSetupArch/Wallpapers/Wallpapers/*
 
+# Commit aks Push
 cd /HDD1/HyprSetupArch/ && git commit -m "sync" -a && git push
--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ echo "Make sure yay is installed before running! Kill the script if it isn't."
 sleep 4
 
 mkdir ~/Pictures/Wallpapers/
-cd ~/Pictures/Wallpapers/ && wget https://github.com/Mato1111/HyprSetupArch/raw/main/Wallpapers/wallpapers.zip
+cd ~/Downloads/ && wget https://github.com/Mato1111/HyprSetupArch/raw/main/Wallpapers/wallpapers978543.zip
+unzip ~/Downloads/wallpapers978543.zip
 
-yay -S hyprland-git wget waybar-hyprland dunst copyq python python-pyqt5 ffmpeg linux-wallpaperengine-git waypaper swaybg polkit-kde-agent qt5ct kvantum layan-gtk-theme-git kora-icon-theme --noconfirm
+yay -R xdg-desktop-portal-kde xdg-desktop-portal-wlr xdg-desktop-portal-dde xdg-desktop-portal-gtk xdg-desktop-portal-lxqt xdg-desktop-portal-xapp xdg-desktop-portal-gnome xdg-desktop-portal-hyprland xdg-desktop-portal-wlr-git 
+yay -S hyprland-git xdg-desktop-portal-hyprland-git wget waybar-hyprland dunst copyq python python-pyqt5 ffmpeg linux-wallpaperengine-git waypaper swaybg polkit-kde-agent qt5ct kvantum layan-gtk-theme-git kora-icon-theme --noconfirm
 Hyprland & waypaper & kvantummanager & qt5ct & waybar &
 sleep 4
 killall Hyprland waypaper kvantummanager qt5ct waybar
