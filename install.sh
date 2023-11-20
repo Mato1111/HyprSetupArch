fc0cc112ca3b621678a369e6e9701a3bb2ad25af
Mato1111 <ratoteamyt@gmail.com>
2023-11-19 20:25:01 -0500

    .

--- install.sh
+++ install.sh
@@ -7,16 +7,19 @@ cd ~/.config/hypr/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupA
 
 cd ~
 
+mkdir ~/Pictures/Wallpapers/
 mkdir ~/.config/waypaper/
 cd ~/.config/~/.config/waypaper/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupArch/main/waypaper/config.ini
 
 yay -S hyprland-git waybar-hyprland dunst copyq python python-pyqt5 ffmpeg linux-wallpaperengine-git waypaper swaybg polkit-kde-agent qt5ct kvantum layan-gtk-theme-git --noconfirm
 
-mkdir ~/Pictures/Wallpapers/
+
 
 
 cd ~/ && wget https://raw.githubusercontent.com/vinceliuice/Layan-kde/master/install.sh && bash install.sh
 
+cd ~/Downloads && wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/add-to-apps.sh & wget https://raw.githubusercontent.com/Mato1111/WallpaperEngine-Waypaper-Selector/main/install.sh
+bash 
 
 gsettings set org.gnome.desktop.interface gtk-theme "Layan-Dark"
 kvantummanager
