e1480339a93d124c603e08bf640d72f9a2f62ed3
Mato1111 <ratoteamyt@gmail.com>
2023-11-19 20:05:07 -0500

    .

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 
 yay -S wget --noconfirm
 mkdir ~/.config/hypr/
-cd ~/.config/hypr/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupArch/main/hyprland.conf
+cd ~/.config/hypr/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupArch/main/hypr/hyprland.conf
 
+cd ~
+
+mkdir ~/.config/waypaper/
+cd ~/.config/~/.config/waypaper/ && wget https://raw.githubusercontent.com/Mato1111/HyprSetupArch/main/waypaper/config.ini
 
 yay -S hyprland-git waybar-hyprland dunst copyq python python-pyqt5 ffmpeg linux-wallpaperengine-git waypaper swaybg polkit-kde-agent qt5ct kvantum layan-gtk-theme-git --noconfirm
 
