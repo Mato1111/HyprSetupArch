99ca92a36eb4fcc1fe2e54d84ba9f75145ba9209
Mato1111 <ratoteamyt@gmail.com>
2023-11-19 20:07:14 -0500

    .

--- Local Sync/sync.sh
+++ Local Sync/sync.sh
@@ -2,6 +2,5 @@
 # -*- coding: utf-8 -*-
 
 rsync ~/.config/hypr/hyprland.conf /HDD1/HyprSetupArch/hypr/hyprland.conf
-rsync ~/.config/waypaper/config.ini /HDD1/HyprSetupArch/waypaper/config.ini
 rsync -r --del ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
 cd /HDD1/HyprSetupArch/ && git commit -m "sync" -a && git push
