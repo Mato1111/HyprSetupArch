705a5a688619dfa1602345551e9d1330fa4d308c
Mato1111 <ratoteamyt@gmail.com>
2023-12-15 15:07:31 -0500

    sync

--- Local Sync/sync.sh
+++ Local Sync/sync.sh
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # Wallpapers
-rsync -r --del --inplace -U -u ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
+rsync -r --del --inplace -U -c ~/Pictures/Wallpapers /HDD1/HyprSetupArch/Wallpapers/
 sleep 0.5
 zip -0 -FS /HDD1/HyprSetupArch/Wallpapers/wallpapers978543.zip /HDD1/HyprSetupArch/Wallpapers/Wallpapers/*
 # Configs
-rsync -r --del --inplace -U -u -p ~/.config/ /HDD1/.config/
+rsync -r --del --inplace -U -c -p ~/.config/ /HDD1/.config/
 rsync ~/.config/hypr/hyprland.conf /HDD1/HyprSetupArch/hypr/hyprland.conf &
 rsync ~/.config/WallpaperSelect/wallpaper.txt /HDD1/HyprSetupArch/WallpaperSelect/wallpaper.txt &
 rsync ~/.config/qt5ct/qt5ct.conf /HDD1/HyprSetupArch/Theme/qt5ct/qt5ct.conf &
