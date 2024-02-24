86505ab18f3fee5413d4f66aedc9b6787c300423
Mato1111 <ratoteamyt@gmail.com>
2024-02-24 05:47:50 -0500

    sync

--- Local Sync/sync copy.sh
+++ Local Sync/sync copy.sh
@@ -2,3 +2,4 @@
 # -*- coding: utf-8 -*-
 
 ffplay ~/.config/hypr/rapidsave.com_i_recreated_the_windows_95_startup_sound_and_also-v3lhhphcu2881.mp3 -nodisp -autoexit -volume 75 &
+sleep 2 && killall dunst
